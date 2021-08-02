91213a9d1867dfce451e46048aa9303117599835
Nagim123 <fourcolorsgame@gmail.com>
2021-08-02 17:05:13 +0300

    Third task uploaded

---
+++ GCDFinderTask3.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int gcd(int a,int b) {
+	if (b == 0) {
+		return a;
+	} else {
+		return gcd(b, a % b);
+	}
+}
+
+int main() {
+	int x,y;
+	cout<<"Type 2 integers:";
+	cin>>x>>y;
+	cout<<"Greatest common divisor:"<<gcd(x,y);
+	return 0;
+}
+
